3b03cc2b551274aa089377c1ef54067775ad795c
Andrey Gurevich <gurevichan@gmail.com>
2025-04-04 13:22:52 +0300

    initial version of stocks

--- stream.py
+++ stream.py
@@ -3,26 +3,8 @@ import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
 
-def get_rand_data():
-    """Load stock data."""
-    df = pd.DataFrame({
-        'date': pd.date_range(start='2023-01-01', periods=365, freq='D'),
-        'Stock A': pd.Series(range(100, 465)),
-        'Stock B': pd.Series(range(200, 565)),
-        'Stock C': pd.Series(range(300, 665))
-    }).set_index('date')
-    # Simulate some stock data
-    df['Stock A'] = df['Stock A'] + (df.index.dayofyear * 0.3)
-    df['Stock B'] = df['Stock B'] + (df.index.dayofyear * 0.1)
-    df['Stock C'] = df['Stock C'] + (df.index.dayofyear * 0.2)
-    # # Add some noise
-    df['Stock A'] += np.random.normal(45, 100, size=len(df)) + np.random.normal(0, 10, size=len(df))
-    df['Stock B'] += np.random.normal(-10, 50, size=len(df)) + np.random.normal(0, 5, size=len(df))
-    df['Stock C'] += np.random.normal(5, 150, size=len(df)) + np.random.normal(0, 15, size=len(df))
-    return df
-
-def load_data():
-    df = pd.read_csv("/Users/andreygurevich/Coding/text.csv", index_col=0, parse_dates=True)
+def load_data(path="/Users/andreygurevich/Coding/test_stocks.csv"):
+    df = pd.read_csv(path, index_col=0, parse_dates=True)
     df.index = pd.to_datetime(df.index)
     return df
 
